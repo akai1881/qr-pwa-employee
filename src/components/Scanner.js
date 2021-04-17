513d6aa3ca1696c5ca49b1a3b213c8cdf1d33eae
akai1881 <akaibegaliev1881@gmail.com>
2021-04-17 10:11:52 +0600

    backup version

--- src/components/Scanner.js
+++ src/components/Scanner.js
@@ -1,10 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import QrReader from 'react-qr-reader';
 import {
-  compareTime,
-  shiftStartedTime,
-  parseScanData,
-  countMinutesLate, getUserLocation
+    compareTime,
+    shiftStartedTime,
+    parseScanData,
+    countMinutesLate, getUserLocation
 } from '../helpers/functions.js';
 import {checkShift, getUserData, setShiftToDb, getCurrentTimeStamp} from '../api';
 import RedCheck from '../assets/images/check_red.svg';
@@ -22,213 +22,218 @@ AOS.init();
 
 
 const Scanner = ({user}) => {
-  const [access, setAccess] = useState(false);
-  const [loader, setLoader] = useState(true);
-  const [userData, setUserData] = useState({});
-  const [shift, setShift] = useState('');
-  const [success, error, locFail, handleLocation] = useLocationToast();
-  const [location, setLocation] = useState(false);
-  const [isLate, setIsLate] = useState(false);
+    const [access, setAccess] = useState(false);
+    const [loader, setLoader] = useState(true);
+    const [userData, setUserData] = useState({});
+    const [shift, setShift] = useState('');
+    const [success, error, locFail, handleLocation] = useLocationToast();
+    const [location, setLocation] = useState(false);
+    const [isLate, setIsLate] = useState(false);
 
 
-  useEffect(() => {
+    useEffect(() => {
 
-    getUserLocation(checkUserLocation, handleLocError);
+        getUserLocation(checkUserLocation, handleLocError);
 
 
-    // check if user already shifted
-    if (user) {
-      fetchUser().then(() => console.log('success'));
+        // check if user already shifted
+        if (user) {
+            fetchUser().then(() => console.log('success'));
+        }
+        checkUserShift().then(() => console.log('success'));
+
+
+    }, [window.navigator.geolocation]);
+
+
+    async function checkUserShift() {
+        try {
+            const res = await checkShift(user.uid);
+            setShift(shiftStartedTime(res.date.toDate()));
+            setIsLate(res.isLate);
+            setAccess(true);
+        } catch (e) {
+            console.log(e.message);
+            setLoader(false);
+        } finally {
+            setLoader(false);
+        }
     }
-    checkUserShift().then(() => console.log('success'));
 
+    const checkUserLocation = (position) => {
+        const {latitude, longitude} = position.coords;
 
-  }, [window.navigator.geolocation]);
+        console.log(polygon);
 
+        const loc = classifyPoint(polygon, [latitude, longitude]);
 
-  async function checkUserShift() {
-    try {
-      const res = await checkShift(user.uid);
-      setShift(shiftStartedTime(res.date.toDate()));
-      setIsLate(res.isLate);
-      setAccess(true);
-    } catch (e) {
-      console.log(e.message);
-      setLoader(false);
-    } finally {
-      setLoader(false);
-    }
-  }
+        console.log(latitude, longitude);
 
-  const checkUserLocation = (position) => {
-    const {latitude, longitude} = position.coords;
-
-    console.log(polygon);
-
-    const loc = classifyPoint(polygon, [latitude, longitude]);
-
-    console.log(latitude, longitude);
-
-    if (loc === -1 || loc === 0) {
-      setLocation(true);
-      handleLocation(SUCCESS);
-      console.log(loc);
-      return;
-    }
-    handleLocation(FAIL);
-    console.log(loc);
-    setLocation(false);
-  };
-
-  async function fetchUser() {
-    const fetchedUser = await getUserData(user.uid);
-
-
-    setUserData({
-      email: user.email,
-      uid: user.uid,
-      firstName: fetchedUser.firstName,
-      lastName: fetchedUser.lastName,
-      role: fetchedUser.role,
-      department: fetchedUser.department.name,
-      phone: fetchedUser.phone1,
-      dueTime: fetchedUser.dueTime.toDate() || new Date()
-    });
-
-  }
-
-  function Alert(props) {
-    return <MuiAlert elevation={6} variant="filled" {...props} />;
-  }
-
-  function handleLocError() {
-    handleLocation(ERROR);
-  }
-
-
-  const handleScan = async result => {
-    if (!result) return;
-
-    const permission = parseScanData(result);
-
-    let date = new Date();
-
-    if (!location || !permission) {
-      return;
-    }
-    setLoader(true);
-    let is_late = false;
-    let minutesLate = 0;
-    try {
-      date = await getCurrentTimeStamp();
-      setShift(shiftStartedTime(date));
-    } catch (e) {
-      console.log(e.message);
-    }
-    if (compareTime(date, userData.dueTime)) {
-      is_late = true;
-      minutesLate = countMinutesLate(date, userData.dueTime);
-    }
-    if (is_late) {
-      setIsLate(true);
-    }
-    const userShift = {
-      ...userData,
-      date,
-      location,
-      isLate: is_late,
-      minutesLate
+        if (loc === -1 || loc === 0) {
+            setLocation(true);
+            handleLocation(SUCCESS);
+            console.log(loc);
+            return;
+        }
+        handleLocation(FAIL);
+        console.log(loc);
+        setLocation(false);
     };
 
-    console.log(userShift);
+    async function fetchUser() {
+        const fetchedUser = await getUserData(user.uid);
 
-    setShiftToDb(date, userShift).then(() => {
-      setAccess(true);
-      setLoader(false);
-    });
-  };
 
-  if (loader) {
+        setUserData({
+            email: user.email,
+            uid: user.uid,
+            firstName: fetchedUser.firstName,
+            lastName: fetchedUser.lastName,
+            role: fetchedUser.role,
+            department: fetchedUser.department.name,
+            phone: fetchedUser.phone1,
+            dueTime: fetchedUser.dueTime.toDate() || new Date()
+        });
+
+    }
+
+    function Alert(props) {
+        return <MuiAlert elevation={6} variant="filled" {...props} />;
+    }
+
+    function handleLocError() {
+        handleLocation(ERROR);
+    }
+
+
+    const handleScan = async result => {
+        if (!result) return;
+
+        const permission = parseScanData(result);
+
+        let date = new Date();
+        let is_late = false;
+        let minutesLate = 0;
+
+
+        if (!location || !permission) {
+            return;
+        }
+
+        setLoader(true);
+
+        try {
+            date = await getCurrentTimeStamp();
+            setShift(shiftStartedTime(date));
+        } catch (e) {
+            console.log(e.message);
+        }
+
+        if (compareTime(date, userData.dueTime)) {
+            is_late = true;
+            minutesLate = countMinutesLate(date, userData.dueTime);
+        }
+
+        if (is_late) {
+            setIsLate(true);
+        }
+        const userShift = {
+            ...userData,
+            date,
+            location,
+            isLate: is_late,
+            minutesLate
+        };
+
+        setShiftToDb(date, userShift).then(() => {
+            setAccess(true);
+            setLoader(false);
+        });
+    };
+
+    if (loader) {
+        return (
+            <div className="loader-container">
+                <Spinner loader={loader}/>
+            </div>
+        );
+    }
+
+
+    const handleCloseError = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        handleLocation(ERROR);
+    };
+
+    const handleCloseSuccess = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        handleLocation(SUCCESS);
+    };
+
+
+    const handleCloseFail = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        handleLocation(FAIL);
+    };
+
+
+    const handleError = error => {
+        console.error(error);
+    };
+
     return (
-      <div className="loader-container">
-        <Spinner loader={loader}/>
-      </div>
-    );
-  }
-
-
-  const handleCloseError = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-    handleLocation(ERROR);
-  };
-
-  const handleCloseSuccess = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-    handleLocation(SUCCESS);
-  };
-
-
-  const handleCloseFail = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-    handleLocation(FAIL);
-  };
-
-
-  const handleError = error => {
-    console.error(error);
-  };
-
-  return (
-    <div className="w-full flex-c-m m-b-50 overlay" style={{height: '100vh'}}>
-      <Snackbar open={locFail} autoHideDuration={3000} onClose={handleCloseFail}
-                anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
-        <Alert onClose={handleCloseFail} severity="warning">
-          Геопозиция не соответcвует <br/> или перезагрузите приложение{location}
-        </Alert>
-      </Snackbar>
-      <Snackbar open={error} autoHideDuration={3000} onClose={handleCloseError}
-                anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
-        <Alert onClose={handleCloseError} severity="error">
-          Предоставьте доступ к вашей локации <br/> или перезагрузите приложение{location}
-        </Alert>
-      </Snackbar>
-      <Snackbar open={success} autoHideDuration={3000} onClose={handleCloseSuccess}
-                anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
-        <Alert onClose={handleCloseSuccess} severity="success">
-          Локация определена{location}
-        </Alert>
-      </Snackbar>
-      {access ? (
-        <div
-          className="paper flex-col-c-m"
-          data-aos="zoom-in-up"
-          data-aos-delay="300"
-          style={{
-            width: '300px',
-            padding: '40px',
-            backgroundColor: '#fff',
-            borderRadius: '10px'
-          }}
-        >
-          {isLate ? (
-            <img data-aos="flip-down" data-aos-delay="600" style={{width: '90px'}} src={RedCheck} alt="Late"/>
-          ) : (
-            <img data-aos="flip-down" data-aos-delay="600" style={{width: '90px'}} src={Check} alt="On time"/>
-          )}
-          <p className="fs-18 text-center m-t-30">{'Вы вошли в систему'}</p>
-          <p className="fs-18">{shift}</p>
+        <div className="w-full flex-c-m m-b-50 overlay" style={{height: '100vh'}}>
+            <Snackbar open={locFail} autoHideDuration={3000} onClose={handleCloseFail}
+                      anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
+                <Alert onClose={handleCloseFail} severity="warning">
+                    Геопозиция не соответcвует <br/> или перезагрузите приложение
+                </Alert>
+            </Snackbar>
+            <Snackbar open={error} autoHideDuration={3000} onClose={handleCloseError}
+                      anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
+                <Alert onClose={handleCloseError} severity="error">
+                    Предоставьте доступ к вашей локации <br/> или перезагрузите приложение
+                </Alert>
+            </Snackbar>
+            <Snackbar open={success} autoHideDuration={3000} onClose={handleCloseSuccess}
+                      anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
+                <Alert onClose={handleCloseSuccess} severity="success">
+                    Локация определена
+                </Alert>
+            </Snackbar>
+            {access ? (
+                <div
+                    className="paper flex-col-c-m"
+                    data-aos="zoom-in-up"
+                    data-aos-delay="300"
+                    style={{
+                        width: '300px',
+                        padding: '40px',
+                        backgroundColor: '#fff',
+                        borderRadius: '10px'
+                    }}
+                >
+                    {isLate ? (
+                        <img data-aos="flip-down" data-aos-delay="600" style={{width: '90px'}} src={RedCheck}
+                             alt="Late"/>
+                    ) : (
+                        <img data-aos="flip-down" data-aos-delay="600" style={{width: '90px'}} src={Check}
+                             alt="On time"/>
+                    )}
+                    <p className="fs-18 text-center m-t-30">{'Вы вошли в систему'}</p>
+                    <p className="fs-18">{shift}</p>
+                </div>
+            ) : (
+                <QrReader delay={800} onError={handleError} onScan={handleScan} className="scanner"/>
+            )}
         </div>
-      ) : (
-        <QrReader delay={800} onError={handleError} onScan={handleScan} className="scanner"/>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Scanner;
--- src/utils/useLocationToast.js
+++ src/utils/useLocationToast.js
@@ -2,28 +2,28 @@ import {useState} from "react";
 import {ERROR, FAIL, SUCCESS} from "../constants";
 
 export const useLocationToast = () => {
-  const [success, setSuccess] = useState(false);
-  const [error, setError] = useState(false);
-  const [locFail, setLocFail] = useState(false);
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
+    const [locFail, setLocFail] = useState(false);
 
-  function handleLocation(status) {
-    switch (status) {
-      case ERROR:
-        setError((prev) => !prev);
-        break;
+    function handleLocation(status) {
+        switch (status) {
+            case ERROR:
+                setError((prev) => !prev);
+                break;
 
-      case SUCCESS:
-        setSuccess((prev) => !prev);
-        break;
+            case SUCCESS:
+                setSuccess((prev) => !prev);
+                break;
 
-      case  FAIL:
-        setLocFail((prev) => !prev);
-        break;
+            case  FAIL:
+                setLocFail((prev) => !prev);
+                break;
 
-      default:
-        return 'default';
+            default:
+                return 'default';
+        }
     }
-  }
 
-  return [success, error, locFail, handleLocation];
+    return [success, error, locFail, handleLocation];
 };
